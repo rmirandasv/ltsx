e312307621dd5490267e68c99584305f7c89dd6a
Ronald Miranda <ronaldmirandasv@gmail.com>
2025-03-26 20:09:05 -0600

    chore: install shadcn

--- resources/ts/pages/home.tsx
+++ resources/ts/pages/home.tsx
@@ -1,7 +1,17 @@
+import { Card, CardContent } from "@/components/ui/card";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1>Home page</h1>
+      <Card>
+        <CardContent>
+          <h1 className="text-2xl font-bold">Welcome to your new app!</h1>
+          <p className="mt-2">
+            This is a simple starter template for your new app. It includes
+            Tailwind CSS, TypeScript, and Laravel.
+          </p>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'resources/ts'),
-        },
+  resolve: {
+    alias: {
+      "@": "/resources/ts",
     },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/ts/app.tsx'],
-            refresh: true,
-            resolve: (name) => {
-                const pages = import.meta.glob("./pages/**/*.tsx", { eager: true });
-                return pages[`./pages/${name}.tsx`];
-            }
-        }),
-        react(),
-        tailwindcss(),
-    ],
-    server: {
-        cors: {
-            origin: 'http://localhost:8000',
-        }
+  },
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/ts/app.tsx"],
+      refresh: true,
+      resolve: (name) => {
+        const pages = import.meta.glob("./pages/**/*.tsx", { eager: true });
+        return pages[`./pages/${name}.tsx`];
+      },
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    cors: {
+      origin: "http://localhost:8000",
     },
+  },
 });
