86af34f83f08ba9d03b0726446d558f4b34b0633
Ronald Miranda <ronaldmirandasv@gmail.com>
2025-03-27 15:16:57 -0600

    home page

---
+++ resources/ts/components/layouts/app-layout.tsx
@@ -0,0 +1,6 @@
+export default function AppLayout() {
+    return (
+        <div className="min-h-screen bg-background antialiased">
+        </div>
+    )
+}
--- resources/ts/pages/home.tsx
+++ resources/ts/pages/home.tsx
@@ -1,17 +1,46 @@
-import { Card, CardContent } from "@/components/ui/card";
+import AppLogo from "@/components/logo/app-logo";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Link } from "@inertiajs/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <Card>
-        <CardContent>
-          <h1 className="text-2xl font-bold">Welcome to your new app!</h1>
-          <p className="mt-2">
-            This is a simple starter template for your new app. It includes
-            Tailwind CSS, TypeScript, and Laravel.
-          </p>
-        </CardContent>
-      </Card>
+    <div className="min-h-screen bg-background antialiased flex flex-col">
+      <div className="px-4 py-3 flex items-center justify-end space-x-3 bg-gray-100">
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+        <Button variant="outline" asChild>
+          <Link href="/register">Register</Link>
+        </Button>
+      </div>
+      <div className="max-w-7xl w-full mx-auto py-6 px-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>LTSX</CardTitle>
+            <CardDescription>
+              Laravel + Tailwind + Inertia + React + TypeScript
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <AppLogo />
+            <p className="mt-4">
+              Welcome to LTSX.
+              <br />
+              This is a starter template for Laravel + Tailwind + Inertia +
+              React + TypeScript.
+              <br />
+              You can start building your application from here.
+            </p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
