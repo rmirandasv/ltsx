c70402f4eed0f12901c4473815feed88d17dbe66
Ronald Miranda <ronaldmirandasv@gmail.com>
2025-03-26 19:52:28 -0600

    chore: install / configure inertia, react, typescript, eslint, prettier and vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/ts'),
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/ts/app.tsx'],
             refresh: true,
+            resolve: (name) => {
+                const pages = import.meta.glob("./pages/**/*.tsx", { eager: true });
+                return pages[`./pages/${name}.tsx`];
+            }
         }),
+        react(),
         tailwindcss(),
     ],
+    server: {
+        cors: {
+            origin: 'http://localhost:8000',
+        }
+    },
 });
